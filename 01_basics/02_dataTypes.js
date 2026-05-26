a8760c22584e7362d82d8ed268b3420427f729eb
Anjan Pajiyar <130601195+coderanjan@users.noreply.github.com>
2026-05-26 07:33:12 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as a newer version
+
+// alert(3+3) we are using nodejs , not browser
+
+// code readibility should be high
+console.log(3+3) ;
+console.log("anjan")
+
+let name ="anjan"
+let age=18
+let isLoggedIn =false
+
+// number => 2 to power 53
+// bigint
+// string  =? ""
+// boolean  => true/false
+// null  => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) //object
