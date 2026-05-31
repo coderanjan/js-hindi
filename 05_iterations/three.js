47a93ec5690e2dcfc7634add933892d8bfe726b1
Anjan Pajiyar <130601195+coderanjan@users.noreply.github.com>
2026-05-31 03:26:19 +0000

    high order array loops

---
+++ 05_iterations/three.js
@@ -0,0 +1,38 @@
+//  for of 
+
+// const arr = [1,2,3,4,5]
+
+// for (const element of arr) {
+//     console.log(element);
+    
+// }
+
+// const greetings = "Hello World!"
+// for (const element of greetings) {
+//     console.log(`Each char os ${element}`);  
+// }
+
+// map
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United States of America")
+map.set('Fr',"France")
+map.set('IN',"India")
+
+// console.log(map);
+
+// for(const [key,value] of map){
+//     console.log(key,':',value);
+    
+// }
+
+// const myobject ={
+//     'game1' : 'NFS',
+//     'game2' : 'SPIDERMAN'
+// }
+
+// for(const [key,value] of myobject){
+//     console.log(key,':',value);
+    
+// }
