1c4505f68a33c87e8db9a18afbc3d33f372749d7
Anjan Pajiyar <130601195+coderanjan@users.noreply.github.com>
2026-05-31 01:47:50 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,39 @@
+//  for 
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i
+//     if(element ==5){
+//         console.log(" ");
+        
+//     }
+//     console.log();
+    
+    
+// }
+
+// for (let i = 0; i < array.length; i++) {
+    
+//     for (let j = 0; j < array.length; j++) {
+        
+        
+//     }
+// }
+
+// let myArray = ["flash","batman","superman"]
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+// }
+
+// break and continue
+
+for (let i = 1; i <= 20; i++) {
+    if(i == 5){
+        console.log("detected 5");
+        continue
+    }
+    console.log(`value of i is ${i}`);
+    
+    
+}
